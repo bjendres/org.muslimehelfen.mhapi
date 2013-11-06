7941ef2af53b4250be719e043c0064b20f4feb12
systopia <endres@systopia.de>
2013-11-06 17:42:39 +0100

    updated example file

--- example.php
+++ example.php
@@ -5,30 +5,41 @@ $mh_civicrm_api_config['url'] = "<your_drupal_URL>/sites/all/modules/civicrm/ext
 $mh_civicrm_api_config['site_key'] = "<civicrm site key>";
 $mh_civicrm_api_config['api_key'] = "<civicrm API key>";
 
-
 $contact_information = array();
 $contact_information['email'] = 'someguy@somewhere.nil';
 $contact_information['first_name'] = 'Björn';
 $contact_information['last_name'] = 'Endres';
+$contact_information['contact_type'] = 'Individual';								// or: "Organization"
+$contact_information['prefix'] = 'Herr';											// or: "Frau"
+$contact_information['street_address'] = 'Franzstr. 122';
+$contact_information['country'] = 'Deutschland';									// or "Österreich", "Schweiz"
+$contact_information['postal_code'] = '53111';
+$contact_information['city'] = 'Bonn';
+$contact_information['phone'] = '0228 96104990';
+$contact_information['city'] = 'Bonn';
 $contact_information['create_if_not_found'] = '1';
-$contact_information['contact_type'] = 'Individual';
+
+
 $contact_info = mh_civicrm_get_contact($contact_information);
 print_r($contact_info);
 
-$contribution_information = array();
-$contribution_information['contact_id'] = $contact_info['id'];
-$contribution_information['financial_type'] = 'Brunnen/Wasser';
-$contribution_information['payment_instrument'] = 'Kreditkarte';
-$contribution_information['contribution_campaign'] = '111111';
-$contribution_information['total_amount'] = '100.00';								// needs to have two postfix digits
-$contribution_information['currency'] = 'EUR';										// or e.g.: 'CHF'
-$contribution_information['contribution_status'] = 'In Bearbeitung';				// or e.g.: 'Abgeschlossen'
-$contribution_information['is_test'] = '1';											// set to '0' for real contributions
+if ($contact_info['id']) {
+	$contribution_information = array();
+	$contribution_information['contact_id'] = $contact_info['id'];
+	$contribution_information['financial_type'] = 'Brunnen/Wasser';
+	$contribution_information['payment_instrument'] = 'Kreditkarte';					
+	$contribution_information['contribution_campaign'] = '111111';
+	$contribution_information['total_amount'] = '100.00';								// needs to have two postfix digits
+	$contribution_information['currency'] = 'EUR';										// or e.g.: 'CHF'
+	$contribution_information['contribution_status'] = 'In Bearbeitung';				// or e.g.: 'Abgeschlossen'
+	$contribution_information['is_test'] = '0';											// set to '0' for real contributions
 
 
-$result = mh_civicrm_create_contribution($contribution_information);
-print_r($result);
-
+	$result = mh_civicrm_create_contribution($contribution_information);
+	print_r($result);
+} else {
+	print_r("Fehler beim der Zuordnung des Kontakts.\n");
+}
 
 
 /*********************************************************
@@ -47,6 +58,8 @@ function mh_civicrm_get_contact($contact_information) {
 	$query['entity'] = 'MhApi';
 	$query['action'] = 'getcontact';
 
+	print_r($query);
+
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_POST, 1);
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
@@ -57,7 +70,6 @@ function mh_civicrm_get_contact($contact_information) {
     curl_setopt($curl, CURLOPT_SSLVERSION, 3);
 
     $response = curl_exec($curl);
-    print_r($response);
     return json_decode($response, true);
 }
 
@@ -72,6 +84,7 @@ function mh_civicrm_create_contribution($contact_information) {
 	$query['version'] = 3;
 	$query['entity'] = 'MhApi';
 	$query['action'] = 'addcontribution';
+	print_r($query);
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_POST, 1);
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
